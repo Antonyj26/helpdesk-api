fed5586d46857cce34585e25d0a655349e02f54e
Antonyj26 <antonyjosu26@gmail.com>
2025-10-20 14:17:20 -0300

    chore: configure CORS to allow requests from the front-end

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { errorHandling } from "@/middlewares/errorHandling";
 import { route } from "@/routes";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(route);
 app.use(errorHandling);
